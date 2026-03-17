b92eeefa1f2bb6284c006742f4893237eb84535b
Vanzhere <vaniya.sharma158@gmail.com>
2026-03-17 08:23:04 +0000

    Initial commit

--- patterns.cpp
+++ patterns.cpp
@@ -18,12 +18,31 @@ void print2(int n){
     }
 
 }
+
+void print3(int n){
+      for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j<< " ";
+        }
+        cout << endl;
+    }
+
+}
+void print4(int n){
+      for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << i<< " ";
+        }
+        cout << endl;
+    }
+
+}
 int main(){
     int t;
     cin >> t;
     for(int i=0;i<t; i++){
         int n;
         cin >> n;
-        print2(n);
+        print4(n);
     }
 }
