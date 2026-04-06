46c76c6d42eeadba4549e671036d18f16308e943
Vanzhere <vaniya.sharma158@gmail.com>
2026-04-06 00:46:04 +0000

    Initial Commit

---
+++ Basics.java
@@ -0,0 +1,77 @@
+// 1. Basic structure
+Public class JavaBasics{
+    public class void main (string args[]){
+
+        System.out.println("Hello World! * 3");
+    }
+}
+// Datatypes
+int a= 9;
+double d=10.1;
+char c= "A";
+boolean m = true;
+
+// Typecasting
+Double x= 10.5;
+// int y= (int)x; ----explicit
+int p=10;
+// double p=q;-----implicit
+
+// Control statements
+1. if/else
+if(a>5){
+    System.out.println("Greater");
+}else{
+    System.out.println("Smaller");
+}
+
+// Switch
+switch(day){
+    case1: System.out.println("Mon"); 
+    break;
+    default: System.out.println("Invalid");
+}
+
+// Loops
+for(int i=0; i>5; i++){}
+
+while(i>5){}
+
+do{
+    i++;
+}while(i<5);
+
+// strings
+String s = "hello";
+String s2 = "World";
+
+s.length();
+s.charAt(0);
+s.substring(1,3);
+s.equals(s2);
+s.toUpperCase();
+
+// arrays
+int[]arr ={1,2,3};
+for (int i=0; i<arr.length; i++){}
+
+// 2D Array
+int [][] mat= new int[2][3];
+
+// Methods
+static int add(int a , int b){
+    return a+b;
+}
+
+// oop concepts---class & objects
+class Car{
+    String name;
+    void drive(){
+        System.out.println("Driving");
+    }
+}
+Car c= new Car();
+c.name= "Range Rover";
+c.drive();
+
+
